6027ea86dd31d5e1d62e6eab774fcc9ba9a4f01b
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-16 10:39:55 +0100

    fix d-m-p version

--- maven/CanaryReleaseStageAndApprovePromote.groovy
+++ maven/CanaryReleaseStageAndApprovePromote.groovy
@@ -49,11 +49,12 @@ node ('swarm'){
 
     def flow = new io.fabric8.Release()
     def fabricMavenPluginVersion = flow.getReleaseVersion "fabric8-maven-plugin"
+    def dockerMavenPluginVersion = '0.13.6'
 
     def canaryVersion = "${versionPrefix}.${env.BUILD_NUMBER}"
     sh "git checkout -b ${env.JOB_NAME}-${canaryVersion}"
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${canaryVersion}"
-    sh 'mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy org.jolokia:docker-maven-plugin:0.13.2:build -Dfabric8.dockerUser=fabric8/ -Ddocker.registryPrefix=docker.io/'
+    sh "mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy org.jolokia:docker-maven-plugin:${dockerMavenPluginVersion}:build -Dfabric8.dockerUser=fabric8/ -Ddocker.registryPrefix=docker.io/"
 
     def fabric8Console = "${env.FABRIC8_CONSOLE ?: ''}"
 
