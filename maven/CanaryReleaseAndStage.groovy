51868c6cd27ceece4ebc8876e9dc4467b8289a39
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-17 11:33:05 +0100

    hard code f-m-p and d-m-p until next relesae where we lookup latest versions

--- maven/CanaryReleaseAndStage.groovy
+++ maven/CanaryReleaseAndStage.groovy
@@ -31,7 +31,7 @@ node ('swarm'){
     // commented until next release 2.2.53
     // def fabricMavenPluginVersion = flow.getMavenCentralVersion "io/fabric8/fabric8-maven-plugin"
     // def dockerMavenPluginVersion = flow.getReleaseVersion "org/jolokia/docker-maven-plugin"
-    def fabricMavenPluginVersion = flow.getReleaseVersion "fabric8-maven-plugin"
+    def fabricMavenPluginVersion = '2.2.52'
     def dockerMavenPluginVersion = '0.13.6'
 
 
