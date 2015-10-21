23be78c921c34a3c71ff13394f96e8f108934fd5
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-21 11:06:27 +0100

    fix compilation error in groovy script

--- maven/CanaryReleaseAndStage.groovy
+++ maven/CanaryReleaseAndStage.groovy
@@ -34,7 +34,7 @@ node ('swarm'){
 
     sh "git checkout -b ${env.JOB_NAME}-${canaryVersion}"
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${canaryVersion}"
-    sh "mvn clean install -U org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy org.jolokia:docker-maven-plugin:${dockerMavenPluginVersion}:build -Dfabric8.dockerUser=fabric8/ -Ddocker.registryPrefix=docker.io/""
+    sh "mvn clean install -U org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy org.jolokia:docker-maven-plugin:${dockerMavenPluginVersion}:build -Dfabric8.dockerUser=fabric8/ -Ddocker.registryPrefix=docker.io/"
 
     // TODO docker push?
 
