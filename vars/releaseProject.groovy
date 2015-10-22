6a2ed28c0049f45c004943c19ca054408ec590c4
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-22 12:05:22 +0100

    push docker images from fabric8 release to staging registry then promote to dockerhub

--- vars/releaseProject.groovy
+++ vars/releaseProject.groovy
@@ -7,18 +7,26 @@ def call(body) {
 
     String versionBumpPullRequest = ""
     def tagDockerImages = []
+    def promoteDockerImages = []
 
     if (config.project == 'ipaas-quickstarts'){
       versionBumpPullRequest = bumpiPaaSQuickstartsVersions{}
+
     } else if (config.project == 'fabric8-ipaas'){
       versionBumpPullRequest = bumpFabric8iPaaSVersions{}
+      promoteDockerImages = ['amqbroker','api-registry','apiman-gateway','apiman','fabric8mq','fabric8mq-consumer','fabric8mq-producer']
+
     } else if (config.project == 'fabric8-devops'){
       versionBumpPullRequest = bumpFabric8DevOpsVersions{}
       tagDockerImages = ['fabric8-console','hubot-irc','eclipse-orion','nexus','gerrit','fabric8-kiwiirc','brackets','jenkins-swarm-client','taiga-front','taiga-back','hubot-slack','lets-chat','jenkernetes']
+      promoteDockerImages = ['chaos-monkey','elasticsearch-logstash-template','fabric8-forge','hubot-notifier','image-linker','kibana-config','prometheus-kubernetes','templates']
+
     } else if (config.project == 'kubernetes-client'){
       versionBumpPullRequest = bumpKubernetesClientVersions{}
+
     } else if (config.project == 'fabric8'){
       versionBumpPullRequest = bumpFabric8Versions{}
+
     }
 
     if (versionBumpPullRequest != null && versionBumpPullRequest != ""){
@@ -37,6 +45,14 @@ def call(body) {
       project = config.project
     }
 
+    if (promoteDockerImages.size() > 0){
+      promoteDockerImages{
+        project = config.project
+        images = promoteDockerImages
+        tag = stagedProject[1]
+      }
+    }
+
     parallel(central: {
       waitUntilArtifactSyncedWithCentral {
         artifact = config.projectArtifact
@@ -44,7 +60,7 @@ def call(body) {
       }
     }, tag: {
       if (tagDockerImages.size() > 0){
-        tagDockerImage{
+        tagDockerImages{
           project = config.project
           images = tagDockerImages
           tag = stagedProject[1]
