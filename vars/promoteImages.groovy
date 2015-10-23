36c54bd3b15f4780854e8d011053d19d3d4a0a0f
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-23 10:31:30 +0100

    use env vars to look up registry to push images to as only pulling images need to use openshift routes

--- vars/promoteImages.groovy
+++ vars/promoteImages.groovy
@@ -1,10 +1,3 @@
-def registry = ""
-try {
-  registry = DOCKER_REGISTRY
-} catch (Throwable e) {
-  registry = "fabric8-docker-registry.${env.DOMAIN}:80/"
-}
-
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -12,14 +5,15 @@ def call(body) {
     body.delegate = config
     body()
 
+
     stage "promote ${config.project} docker images"
     for(int i = 0; i < config.images.size(); i++){
       node ('swarm'){
         ws ('tag'){
           image = config.images[i]
 
-          sh "docker pull ${registry}/fabric8/${image}:${config.tag}"
-          sh "docker tag -f ${registry}/fabric8/${image}:${config.tag} docker.io/fabric8/${image}:${config.tag}"
+          sh "docker pull ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag}"
+          sh "docker tag -f ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag} docker.io/fabric8/${image}:${config.tag}"
 
           retry (3){
             sh "docker push -f docker.io/fabric8/${image}:${config.tag}"
