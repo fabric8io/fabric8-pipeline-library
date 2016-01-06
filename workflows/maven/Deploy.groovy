596b50a218120e704b294d19cad06491aa20bf81
rawlingsj <rawlingsj80@gmail.com>
2016-01-06 23:33:48 +0000

    jenkinsfiles moved to subprojects with readmes

--- workflows/maven/Deploy.groovy
+++
@@ -1,7 +0,0 @@
-node ('kubernetes'){
-  git GIT_URL
-  withEnv(["PATH+MAVEN=${tool 'maven-3.3.1'}/bin"]) {
-
-    sh 'mvn clean install org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy'
-  }
-}
