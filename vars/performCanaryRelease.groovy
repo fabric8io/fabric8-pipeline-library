bdf205d28b3fae0a8f01d779bedb5d4b4de474d5
rawlingsj <rawlingsj80@gmail.com>
2016-05-13 15:52:24 +0100

    only push docker images if we are running on a multi node cluster

--- vars/performCanaryRelease.groovy
+++ vars/performCanaryRelease.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/groovy
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import io.fabric8.kubernetes.client.KubernetesClient
 
 def call(body) {
     // evaluate the body block, and collect configuration into the object
@@ -14,7 +16,22 @@ def call(body) {
 
     kubernetes.image().withName("${env.JOB_NAME}").build().fromPath(".")
     kubernetes.image().withName("${env.JOB_NAME}").tag().inRepository("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${env.JOB_NAME}").withTag(newVersion)
-    kubernetes.image().withName("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${env.JOB_NAME}").push().withTag(newVersion).toRegistry()
+
+    if (isSingleNode()){
+        echo 'Running on a single node, skipping docker push as not needed'
+    } else {
+        kubernetes.image().withName("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${env.JOB_NAME}").push().withTag(newVersion).toRegistry()
+    }
 
     return newVersion
+  }
+
+  @NonCPS
+  def isSingleNode() {
+    KubernetesClient kubernetes = new DefaultKubernetesClient();
+    if (kubernetes.nodes().list().getItems().size() == 1){
+      return true
+    } else {
+      return false
+    }
   }
