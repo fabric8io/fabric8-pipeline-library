e642254080a73dc4589d4d5594bccc4dcb87f467
rawlingsj <rawlingsj80@gmail.com>
2016-06-20 13:38:53 +0100

    lets push java images to the internal docker registry relates to fabric8io/fabric8#5923

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -12,7 +12,8 @@ def call(body) {
     sh "git checkout -b ${env.JOB_NAME}-${config.version}"
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -DnewVersion=${config.version}"
     sh "mvn clean"
-    sh "mvn install -U org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy io.fabric8:docker-maven-plugin:${dockerMavenPluginVersion}:build -Dfabric8.dockerUser=fabric8/"
+    sh "mvn install -U org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy io.fabric8:docker-maven-plugin:${dockerMavenPluginVersion}:build io.fabric8:docker-maven-plugin:${dockerMavenPluginVersion}:push -Ddocker.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT} -Dfabric8.dockerUser=fabric8/"
+    env.DEFAULT_DOCKER_REGISTRY = env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST + ':' + env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT
     try {
       sh 'mvn site site:deploy'
     } catch (err) {
