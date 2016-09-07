55f45a0370b8a7d21ac0494bccee01e842f8d0d5
rawlingsj <rawlingsj80@gmail.com>
2016-09-07 11:44:20 +0100

    switch to using f-m-p 3.x in maven pipelines fixes fabric8io/fabric8#6314

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -7,7 +7,7 @@ def call(body) {
     body()
 
     def flow = new io.fabric8.Fabric8Commands()
-    def dockerMavenPluginVersion = flow.getReleaseVersion "io/fabric8/docker-maven-plugin"
+    def fabric8MavenPluginVersion = flow.getReleaseVersion "io/fabric8/fabric8-maven-plugin"
 
     sh "git checkout -b ${env.JOB_NAME}-${config.version}"
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${config.version}"
@@ -16,21 +16,15 @@ def call(body) {
     Model m = readMavenPom file: 'pom.xml'
     def groupId = m.groupId.split( '\\.' )
     def user = groupId[groupId.size()-1].trim()
+    def artifactId = m.artifactId
 
-    sh "mvn install -U org.apache.maven.plugins:maven-deploy-plugin:2.8.2:deploy io.fabric8:docker-maven-plugin:${dockerMavenPluginVersion}:build -Dfabric8.image=${user}/${env.JOB_NAME}:${config.version} -Dspring.boot.name=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${env.JOB_NAME}:${config.version} -Dfabric8.dockerUser=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/"
-
-    // until we port the quickstarts to use the new f-m-p we need to force a tag and push to a registry if running in a cluster
-
-    kubernetes.image().withName("${user}/${env.JOB_NAME}:${config.version}").tag().inRepository("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${env.JOB_NAME}").withTag("${config.version}")
+    sh "mvn package -U io.fabric8:fabric8-maven-plugin:${fabric8MavenPluginVersion}:build"
 
     if (flow.isSingleNode()){
         echo 'Running on a single node, skipping docker push as not needed'
 
     } else {
-
-      // couldnt get the docker:push to work, maybe it will work after switching to fabric8:push
-      //sh "mvn io.fabric8:docker-maven-plugin:${dockerMavenPluginVersion}:push -Ddocker.push.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT} -Dspring.boot.name=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${env.JOB_NAME}:${config.version} -Dfabric8.dockerUser=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/"
-      kubernetes.image().withName("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${env.JOB_NAME}").push().withTag("${config.version}").toRegistry()
+      sh "mvn io.fabric8:fabric8-maven-plugin:${fabric8MavenPluginVersion}:push -Ddocker.push.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}"
     }
 
     if (flow.hasService("content-repository")) {
