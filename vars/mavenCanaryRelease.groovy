1b6b686028e075ab0f9814e235e99a692b39af44
rawlingsj <rawlingsj80@gmail.com>
2016-09-13 16:32:02 +0100

    tag the local docker image with the registry prefix if we dont push it to the registry

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -12,16 +12,16 @@ def call(body) {
     sh "git checkout -b ${env.JOB_NAME}-${config.version}"
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${config.version}"
     sh "mvn clean"
-
-    Model m = readMavenPom file: 'pom.xml'
-    def groupId = m.groupId.split( '\\.' )
-    def user = groupId[groupId.size()-1].trim()
-    def artifactId = m.artifactId
-
     sh "mvn package -U io.fabric8:fabric8-maven-plugin:${fabric8MavenPluginVersion}:build"
 
     if (flow.isSingleNode()){
         echo 'Running on a single node, skipping docker push as not needed'
+        Model m = readMavenPom file: 'pom.xml'
+        def groupId = m.groupId.split( '\\.' )
+        def user = groupId[groupId.size()-1].trim()
+        def artifactId = m.artifactId
+
+        kubernetes.image().withName("${user}/${artifactId}:${config.version}").tag().inRepository("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${user}/${artifactId}").withTag("${config.version}")
 
     } else {
       sh "mvn io.fabric8:fabric8-maven-plugin:${fabric8MavenPluginVersion}:push -Ddocker.push.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}"
