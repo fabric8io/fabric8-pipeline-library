2feb8b276aa8b07a1d83a778d74f1f5afa3a9ae3
rawlingsj <rawlingsj80@gmail.com>
2016-11-03 13:03:29 +0000

    lets always use teh -e flag when running mvn install so we get error stacktraces

--- src/io/fabric8/Fabric8Commands.groovy
+++ src/io/fabric8/Fabric8Commands.groovy
@@ -212,7 +212,7 @@ def getNewVersionFromTag(pomVersion = null){
 def stageSonartypeRepo () {
   try {
     sh "mvn clean -B"
-    sh "mvn -V -B -U install org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:deploy -P release -DnexusUrl=https://oss.sonatype.org -DserverId=oss-sonatype-staging -Ddocker.push.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}"
+    sh "mvn -V -B -e -U install org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:deploy -P release -DnexusUrl=https://oss.sonatype.org -DserverId=oss-sonatype-staging -Ddocker.push.registry=${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}"
     step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
 
   } catch (err) {
