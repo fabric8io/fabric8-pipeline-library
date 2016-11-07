2eab76d69a518a49ca09eb54dee25c5a3549a043
rawlingsj <rawlingsj80@gmail.com>
2016-11-07 16:56:34 +0000

    get the current namespace from a groovy function now that we dont have env vars from kubernetes-plugin config

--- vars/performCanaryRelease.groovy
+++ vars/performCanaryRelease.groovy
@@ -14,16 +14,19 @@ def call(body) {
     }
 
     def flow = new io.fabric8.Fabric8Commands()
+    def utils = new io.fabric8.Utils()
+    def namespace = utils.getNamespace()
+    def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${env.JOB_NAME}"
 
     env.setProperty('VERSION',newVersion)
 
     kubernetes.image().withName("${env.JOB_NAME}").build().fromPath(".")
-    kubernetes.image().withName("${env.JOB_NAME}").tag().inRepository("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${env.JOB_NAME}").withTag(newVersion)
+    kubernetes.image().withName("${env.JOB_NAME}").tag().inRepository(newImageName).withTag(newVersion)
 
     if (flow.isSingleNode()){
         echo 'Running on a single node, skipping docker push as not needed'
     } else {
-        kubernetes.image().withName("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${env.JOB_NAME}").push().withTag(newVersion).toRegistry()
+        kubernetes.image().withName(newImageName).push().withTag(newVersion).toRegistry()
     }
 
     return newVersion
