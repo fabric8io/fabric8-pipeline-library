6b0dcbdc315fc9e3e70b5b62e5099f5235d97f78
rawlingsj <rawlingsj80@gmail.com>
2016-11-07 19:07:47 +0000

    use the docker client binary rather than the kubernetes-pipeline-plugin as we are getting errors and unable to track them down, the plugin only updates the jenkins console after the job finishes

--- vars/performCanaryRelease.groovy
+++ vars/performCanaryRelease.groovy
@@ -6,28 +6,30 @@ def call(body) {
     body.delegate = config
     body()
 
-    def newVersion = ''
-    if (config.version == '') {
-        newVersion = getNewVersion {}
-    } else {
-        newVersion = config.version
+
+    container('client') {
+      def newVersion = ''
+      if (config.version == '') {
+          newVersion = getNewVersion {}
+      } else {
+          newVersion = config.version
+      }
+
+      def flow = new io.fabric8.Fabric8Commands()
+      def utils = new io.fabric8.Utils()
+      def namespace = utils.getNamespace()
+      echo 'got namespace ' + namespace
+      def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${env.JOB_NAME}:${newVersion}"
+
+      env.setProperty('VERSION',newVersion)
+
+      sh "docker build -t ${newImageName} ."
+      if (flow.isSingleNode()){
+          sh "echo 'Running on a single node, skipping docker push as not needed'"
+      } else {
+          sh "docker push ${newImageName}"
+      }
+      return newVersion
     }
 
-    def flow = new io.fabric8.Fabric8Commands()
-    def utils = new io.fabric8.Utils()
-    def namespace = utils.getNamespace()
-    def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${env.JOB_NAME}"
-
-    env.setProperty('VERSION',newVersion)
-
-    kubernetes.image().withName("${env.JOB_NAME}").build().fromPath(".")
-    kubernetes.image().withName("${env.JOB_NAME}").tag().inRepository(newImageName).withTag(newVersion)
-
-    if (flow.isSingleNode()){
-        echo 'Running on a single node, skipping docker push as not needed'
-    } else {
-        kubernetes.image().withName(newImageName).push().withTag(newVersion).toRegistry()
-    }
-
-    return newVersion
   }
