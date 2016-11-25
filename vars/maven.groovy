1ede35322264c1725f456c16a05fd96038568a30
Ioannis Canellos <iocanel@gmail.com>
2016-11-25 16:53:42 +0200

    Add echo message in maven function.

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,6 +5,7 @@ def call(Map parameters = [:], body) {
     def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:2.2.297')
     def profiles = parameters.get('profiles', 'base')
 
+    echo "Creating maven pod template with image: ${mavenImage} and profiles: ${profiles}."
     def label = "buildpod.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
     podTemplate(label: label, inhertiFrom: "${profiles}", containers: [
             [name: 'maven', image: "${mavenImage}", command: 'cat', ttyEnabled: true, envVars: [
