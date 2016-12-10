3a1c2839cab31e2620b556e06b98c9429b3209af
rawlingsj <rawlingsj80@gmail.com>
2016-12-10 19:47:58 +0000

    only use staging repo if we run in a cluster

--- vars/promoteImages.groovy
+++ vars/promoteImages.groovy
@@ -12,21 +12,18 @@ def call(body) {
 
         image = config.images[i]
 
-        sh "docker pull ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag}"
-        sh "docker tag ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${config.org}/${image}:${config.tag} ${config.toRegistry}/${config.org}/${image}:${config.tag}"
-
+        def flow = new io.fabric8.Fabric8Commands()
+        // if we're running on a single node then we already have the image on this host so no need to pull image
+        if (flow.isSingleNode()){
+            sh "docker tag ${config.org}/${image}:${config.tag} ${config.toRegistry}/${config.org}/${image}:${config.tag}"
+        } else {
+            sh "docker pull ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag}"
+            sh "docker tag ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${config.org}/${image}:${config.tag} ${config.toRegistry}/${config.org}/${image}:${config.tag}"
+        }
+        
         retry (3){
           sh "docker push ${config.toRegistry}/${config.org}/${image}:${config.tag}"
         }
-
-            // NOT YET IMPLEMENTED - WE NEED TO SWITCH TO OPENSHIFT BINARY BUILDER SO OC SHOULD PROBABLY DO THE IMAGE TAGGING
-            // WE HIT AN ISSUE WITH KUBERNETES-WORKFLOW WHEN TAGGING AN IMAGE NOT BUILT ON THE SAME NODE BUT NO WAY TO PULL THE IMAGE FIRST USING KUBERNETES-WORKFLOW.
-            // NOT SURE IF THATS A PROBLEM THOUGH
-            // kubernetes.image().withName("${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${config.org}/${image}:${config.tag}").tag().force().inRepository("${config.toRegistry}/${config.org}/${image}").withTag("${config.tag}")
-            // retry (3){
-            //   kubernetes.image().withName("${config.toRegistry}/${config.org}/${image}").push().force().withTag("${config.tag}").toRegistry()
-            // }
-
       }
     }
   }
