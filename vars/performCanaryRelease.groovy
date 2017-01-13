b6045936e242df50bbe2de575c1644e65ab326b3
rawlingsj <rawlingsj80@gmail.com>
2017-01-13 17:32:36 +0000

    format

--- vars/performCanaryRelease.groovy
+++ vars/performCanaryRelease.groovy
@@ -1,35 +1,36 @@
 #!/usr/bin/groovy
+
 def call(body) {
     // evaluate the body block, and collect configuration into the object
-    def config = [version:'']
+    def config = [version: '']
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
 
     container('clients') {
-      def newVersion = ''
-      if (config.version == '') {
-          newVersion = getNewVersion {}
-      } else {
-          newVersion = config.version
-      }
+        def newVersion = ''
+        if (config.version == '') {
+            newVersion = getNewVersion {}
+        } else {
+            newVersion = config.version
+        }
 
-      def flow = new io.fabric8.Fabric8Commands()
-      def utils = new io.fabric8.Utils()
-      def namespace = utils.getNamespace()
-      echo 'namespace: ' + namespace
-      def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${env.JOB_NAME}:${newVersion}"
+        def flow = new io.fabric8.Fabric8Commands()
+        def utils = new io.fabric8.Utils()
+        def namespace = utils.getNamespace()
+        echo 'namespace: ' + namespace
+        def newImageName = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${namespace}/${env.JOB_NAME}:${newVersion}"
 
-      env.setProperty('VERSION',newVersion)
+        env.setProperty('VERSION', newVersion)
 
-      sh "docker build -t ${newImageName} ."
-      if (flow.isSingleNode()){
-          sh "echo 'Running on a single node, skipping docker push as not needed'"
-      } else {
-          sh "docker push ${newImageName}"
-      }
-      return newVersion
+        sh "docker build -t ${newImageName} ."
+        if (flow.isSingleNode()) {
+            sh "echo 'Running on a single node, skipping docker push as not needed'"
+        } else {
+            sh "docker push ${newImageName}"
+        }
+        return newVersion
     }
 
-  }
+}
