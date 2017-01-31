9c1f34389ddc50876aa373e973839315038e94a9
rawlingsj <rawlingsj80@gmail.com>
2017-01-31 18:33:49 +0100

    change location of docker config

--- vars/nodejsTemplate.groovy
+++ vars/nodejsTemplate.groovy
@@ -29,13 +29,13 @@ def call(Map parameters = [:], body) {
                 containers: [
                         [name: 'nodejs', image: "${nodejsImage}", command: 'cat', ttyEnabled: true],
                         [name: 'clients', image: "${clientsImage}", command: 'cat', ttyEnabled: true, privileged: true]],
-                volumes: [secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                volumes: [secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/root/.docker'),
                           secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                           secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
                           secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
                           secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
                           hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-                envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
+                envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]
         ) {
 
             body(
