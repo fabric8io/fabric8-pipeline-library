1a14070a95a7646719532e5cd42cd93a9fb0d462
rawlingsj <rawlingsj80@gmail.com>
2017-02-03 10:03:36 +0000

    add a function we can use for CI on golang projects

---
+++ vars/goMake.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/groovy
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+
+    def ghOrg =  config.githubOrganisation
+    def dockerOrg = config.dockerOrganisation
+    def prj = config.project
+
+    if (!ghOrg){
+        error 'no github organisation defined'
+    }
+    if (!dockerOrg){
+        error 'no docker organisation defined'
+    }
+    if (!prj){
+        error 'no project defined'
+    }
+
+    def buildPath = "/home/jenkins/go/src/github.com/${ghOrg}/${prj}"
+
+    sh "mkdir -p ${buildPath}"
+
+    dir(buildPath) {
+        git "https://github.com/${ghOrg}/${prj}.git"
+
+
+        container(name: 'go') {
+            stage ('build binary'){
+                sh "make"
+            }
+        }
+
+        if (fileExists('Dockerfile')){
+            container(name: 'docker') {
+                def imageName = "docker.io/${dockerOrg}/${prj}"
+
+                stage ('build image'){
+                    sh "docker build -t ${imageName}:latest ."
+                }
+            }
+        }
+    }
+  }
