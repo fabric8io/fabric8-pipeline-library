904425256acc1f8c7aa3f0275ba0ed6241ded7c1
rawlingsj <rawlingsj80@gmail.com>
2017-02-06 16:51:14 +0000

    add a deploy node

---
+++ vars/deployTemplate.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/groovy
+
+def call(Map parameters = [: ], body) {
+
+    def defaultLabel = "clients.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
+    def label = parameters.get('label', defaultLabel)
+
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.5')
+    def inheritFrom = parameters.get('inheritFrom', 'base')
+    podTemplate(label: label, serviceAccount: 'jenkins', inheritFrom: "${inheritFrom}",
+        containers: [
+            [name: 'clients', image: "${clientsImage}", command: 'cat', ttyEnabled: true]
+        ],
+        volumes: [
+            secretVolume(secretName: 'gke-service-account', mountPath: '/root/home/.gke')
+        ]) {
+        body()
+    }
+}
