8b2d7884de1c449e9e52595bbb78d87b00cd1cd4
rawlingsj <rawlingsj80@gmail.com>
2017-02-08 16:23:04 +0000

    bump clients builder image

--- vars/deployTemplate.groovy
+++ vars/deployTemplate.groovy
@@ -5,7 +5,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = "clients.${env.JOB_NAME}.${env.BUILD_NUMBER}".replace('-', '_').replace('/', '_')
     def label = parameters.get('label', defaultLabel)
 
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.5')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:0.6')
     def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:2.2.297')
     def inheritFrom = parameters.get('inheritFrom', 'base')
     podTemplate(label: label, serviceAccount: 'jenkins', inheritFrom: "${inheritFrom}",
