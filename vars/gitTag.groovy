7b09dd632f52ac766d5f522dc174004bde651e96
rawlingsj <rawlingsj80@gmail.com>
2017-03-20 13:04:06 +0000

    add option to skip the v prefix in a git tag

--- vars/gitTag.groovy
+++ vars/gitTag.groovy
@@ -6,13 +6,18 @@ def call(body) {
     body.delegate = config
     body()
 
+    def skipVersionPrefix = config.skipVersionPrefix ?: false
     sh 'chmod 600 /root/.ssh-git/ssh-key'
     sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
     sh 'chmod 700 /root/.ssh-git'
-    
+
     sh "git config user.email fabric8-admin@googlegroups.com"
     sh "git config user.name fabric8"
 
-    sh "git tag -fa v${config.releaseVersion} -m 'Release version ${config.releaseVersion}'"
+    if (skipVersionPrefix){
+        sh "git tag -fa ${config.releaseVersion} -m 'Release version ${config.releaseVersion}'"
+    } else {
+        sh "git tag -fa v${config.releaseVersion} -m 'Release version ${config.releaseVersion}'"
+    }
     sh "git push origin v${config.releaseVersion}"
 }
