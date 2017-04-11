4cb48adb4c70c9ad405ed10299fcb30950599bdd
Rob Davies <rajdavies@gmail.com>
2017-04-11 13:55:20 +0100

    wrap junitResults in try catch (#195)

--- vars/junitResults.groovy
+++ vars/junitResults.groovy
@@ -6,15 +6,19 @@ def call(body) {
     body()
     def archiveTestResults = config.archiveTestResults ?: true
     if (archiveTestResults) {
-        def surefire = findFiles(glob: '**/surefire-reports/*.xml')
-        if (surefire) {
-            step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0])
-        }
+        try {
+            def surefire = findFiles(glob: '**/surefire-reports/*.xml')
+            if (surefire) {
+                step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0])
+            }
 
 
-        def failsafe = findFiles(glob: '**/failsafe-reports/*.xml')
-        if (failsafe) {
-            step([$class: 'JUnitResultArchiver', testResults: '**/failsafe-reports/*.xml', healthScaleFactor: 1.0])
+            def failsafe = findFiles(glob: '**/failsafe-reports/*.xml')
+            if (failsafe) {
+                step([$class: 'JUnitResultArchiver', testResults: '**/failsafe-reports/*.xml', healthScaleFactor: 1.0])
+            }
+        }catch(err){
+            echo "Failed to find test results "
         }
 
     }
