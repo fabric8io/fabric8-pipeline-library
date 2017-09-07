bd8db6b75bf9236a43d0ff8906041761229e1bf4
rawlingsj <rawlingsj80@gmail.com>
2017-09-07 18:49:21 +0100

    avoid mounting maven repo for release pod template on kubernetes so we can run multiple jobs

--- vars/releaseTemplate.groovy
+++ vars/releaseTemplate.groovy
@@ -47,7 +47,6 @@ def call(Map parameters = [:], body) {
                                 ])
                 ],
                 volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                          persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
                           secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
                           secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
                           secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
