ab5733e35a582ef04f15c514e2999aba6abed219
James Strachan <james.strachan@gmail.com>
2017-10-12 10:03:33 +0100

    upgrade to the latest builder clients

--- vars/goTemplate.groovy
+++ vars/goTemplate.groovy
@@ -7,7 +7,7 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def goImage = parameters.get('goImage', 'fabric8/go-builder:1.8.1.2')
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:vf422ab0')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
     def inheritFrom = parameters.get('inheritFrom', 'base')
     def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:0.0.1' : 'jenkinsci/jnlp-slave:2.62'
 
--- vars/releaseTemplate.groovy
+++ vars/releaseTemplate.groovy
@@ -7,7 +7,7 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:v7973e33')
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:vf422ab0')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
     def dockerImage = parameters.get('dockerImage', 'docker:1.11')
     def inheritFrom = parameters.get('inheritFrom', 'base')
     def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:0.0.1' : 'jenkinsci/jnlp-slave:2.62'
