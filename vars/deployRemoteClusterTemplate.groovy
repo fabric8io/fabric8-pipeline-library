4b496e249bc7e1609534f13c3ee4151fdf7485c0
rawlingsj <rawlingsj80@gmail.com>
2017-10-19 10:49:25 +0100

    add function for deploying snapshots to kubernetes clusters

--- vars/deployRemoteClusterTemplate.groovy
+++ vars/deployRemoteClusterTemplate.groovy
@@ -6,7 +6,7 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('clients')
     def label = parameters.get('label', defaultLabel)
 
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v587cb6c')
     def inheritFrom = parameters.get('inheritFrom', 'base')
     def containerName = parameters.get('containerName', 'clients')
     def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:0.0.1' : 'jenkinsci/jnlp-slave:2.62'
