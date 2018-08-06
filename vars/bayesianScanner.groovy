f464fb8e881e6024482ba242885604932d3384fc
yzainee <37098367+yzainee@users.noreply.github.com>
2018-08-06 19:25:10 +0530

    Adds logic to generate dependency files for Analytics
    While calling the jenkins plugin (Bayesian Analysis), 2 new mvn
    commands are added for generating direct & transitive dependency files.
    
    When Jenkins API invokes the Analytics Server API. These
    changes ensures that when Analytics need not clone and redo
    all the logic to generate the dependencies. Furthermore, this ensures
    that reports are generated for versions included in the build.
    
    Resolves: https://github.com/openshiftio/openshift.io/issues/3982

--- vars/bayesianScanner.groovy
+++ vars/bayesianScanner.groovy
@@ -14,10 +14,14 @@ def call(body) {
         echo "Checking ${serviceName} exists"
         if (flow.hasService(serviceName)) {
             try {
-                sh 'mvn io.github.stackinfo:stackinfo-maven-plugin:0.2:prepare'
+                sh '''
+                    mvn io.github.stackinfo:stackinfo-maven-plugin:0.2:prepare 
+                    mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:resolve -DoutputFile=direct-dependencies.txt -DincludeScope=runtime -DexcludeTransitive=true 
+                    mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:resolve -DoutputFile=transitive-dependencies.txt -DincludeScope=runtime -DexcludeTransitive=false
+                   '''
                 retry(3) {
                     def project = flow.getGitHubProject()
-                    def response = bayesianAnalysis url: 'https://bayesian-link', gitUrl: "git@github.com:${project}.git"
+                    def response = bayesianAnalysis url: 'https://bayesian-link', gitUrl: "https://github.com/${project}.git"
                     if (response.success) {
                         utils.addAnnotationToBuild('fabric8.io/bayesian.analysisUrl', response.getAnalysisUrl())
                     } else {
