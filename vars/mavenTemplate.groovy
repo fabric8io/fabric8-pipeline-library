d8cf396d3a9caecd67a0dca1e0525aabab9a3163
Kishan Sagathiya <kishansagathiya@gmail.com>
2018-10-09 09:33:22 +0530

    Issue #4007 Check if master is reachable (#434)
    Update to use the latest slave image that contains logic to check if
    the network is reliable i.e.,
    https://github.com/fabric8-jenkins/jenkins-openshift-base/pull/47

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -10,7 +10,7 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:v9ff62e0')
-    def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:vb0268ae' : 'jenkinsci/jnlp-slave:2.62'
+    def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:v2132422' : 'jenkinsci/jnlp-slave:2.62'
     def inheritFrom = parameters.get('inheritFrom', 'base')
 
 
