1d4b07e36c87a64f32dab9ede5e119fd9aae367b
Kishan Sagathiya <kishansagathiya@gmail.com>
2018-10-30 18:08:12 +0530

    Check if nodes directory exists, if not error out and exit.
    Check if nodes directory exists, if not error out and exit.
    
    /nodes directory stores data related to slave nodes. Without this we
    can not start slave nodes. Thus stopping the build process.
    
    Issue: https://github.com/openshiftio/openshift.io/issues/4020
    
    Refs: `println` had problems, Be safe and avoid `println`
      - https://issues.jenkins-ci.org/browse/JENKINS-41953
      - https://issues.jenkins-ci.org/browse/JENKINS-23080

--- vars/mavenNode.groovy
+++ vars/mavenNode.groovy
@@ -5,6 +5,18 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('maven')
     def label = parameters.get('label', defaultLabel)
 
+    def nodesDir = "/var/lib/jenkins/nodes"
+    def file = new File(nodesDir)
+    
+    if(file.isDirectory()){
+        echo "$nodesDir exists and it's a directory"
+    } else {
+        error """
+        Directory $nodesDir does not exist. /nodes directory stores data for managing slave nodes.
+        Without this directory we can't start slave nodes, thus this stops us from finishing the build process.
+        """
+    }
+    
     mavenTemplate(parameters) {
         node(label) {
             body()
